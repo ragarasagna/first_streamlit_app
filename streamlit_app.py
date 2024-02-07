305f7139725d389c171d146d52e7f0ef20806b2d
ragarasagna <51982703+ragarasagna@users.noreply.github.com>
2024-02-07 14:51:50 -0600

    Update streamlit_app.py
    breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Masala Oats')
+streamlit.text('Blueberry Muffins')
+streamlit.text('Kale and Egg Salad')
+streamlit.text('Avocado Toast')
